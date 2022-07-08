c7ac4dbdf7a822fe32a0bb7faf41e9bebf0b1d94
Denis Carriere <carriere.denis@gmail.com>
2022-07-08 10:32:12 -0400

    add cors

--- pages/api/[chain]/v1/chain/get_info.ts
+++ pages/api/[chain]/v1/chain/get_info.ts
@@ -1,8 +1,12 @@
+// import NextCors from "nextjs-cors"
 import { NextRequest, NextResponse } from 'next/server';
 import { core } from "@utils/config"
 
-export default async (req: NextRequest ) => {
-  const headers = {'Cache-Control': 's-maxage=1, stale-while-revalidate=59'};
+export default async (req: NextRequest) => {
+  const headers = {
+    'Cache-Control': 's-maxage=1, stale-while-revalidate=59',
+    'Access-Control-Allow-Origin': '*'
+  };
   const info = await core["eos"].v1.chain.get_info();
   return NextResponse.json(info, {headers});
 }
--- pages/api/geo.ts
+++ pages/api/geo.ts
@@ -1,7 +1,10 @@
 import { NextRequest, NextResponse } from 'next/server';
 
 export default async (req: NextRequest ) => {
-  const headers = {'Cache-Control': 's-maxage=1, stale-while-revalidate=59'};
+  const headers = {
+    'Cache-Control': 's-maxage=1, stale-while-revalidate=59',
+    'Access-Control-Allow-Origin': '*'
+  };
   return NextResponse.json(req.geo, {headers});
 }
 
