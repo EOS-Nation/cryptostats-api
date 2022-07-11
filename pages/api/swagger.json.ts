d028ca09e62bd6a3f54506666a7496da78893048
Denis Carriere <carriere.denis@gmail.com>
2022-07-11 14:03:35 -0400

    upgrade next

--- pages/api/swagger.json.ts
+++ pages/api/swagger.json.ts
@@ -4,9 +4,22 @@ const swaggerHandler = withSwagger({
   definition: {
     openapi: '3.0.0',
     info: {
-      title: 'Pinax CryptoStats API',
-      version: '0.1.0',
+      title: 'NextJS Swagger',
     },
+    tags: [
+      {
+        name: "Fees",
+        description: "Total fees paid to a protocol on a given day.",
+        externalDocs: { description: "Find out more", url: "https://cryptostats.community/discover/fees" },
+        version: '0.1.0'
+      },
+      {
+        name: "Issuance",
+        description: "The amount of tokens issued in the past day, in USD.",
+        externalDocs: { description: "Find out more", url: "https://cryptostats.community/discover/issuance" },
+        version: '0.1.0'
+      }
+    ]
   },
   schemaFolders: ['models'],
   apiFolder: 'pages/api',
