dfcd38933b5e6f23a43d7c740f2985390f2c3c0c
Denis Carriere <carriere.denis@gmail.com>
2022-07-20 16:05:12 -0400

    add cors swagger

--- pages/api/swagger.json.ts
+++ pages/api/swagger.json.ts
@@ -1,30 +1,37 @@
-import { withSwagger } from 'next-swagger-doc';
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { setCache } from "@utils/utils"
+import { createSwaggerSpec } from 'next-swagger-doc';
+import * as pkg from "../../package.json";
 
-const swaggerHandler = withSwagger({
-  definition: {
-    openapi: '3.0.0',
-    info: {
-      title: 'EOSIO CryptoStats API',
-      description: "One neutral source of truth for crypto metrics. Used by everyone, managed by the community.",
-      contact: { url: "https://forum.cryptostats.community" },
-      license: { name: "Apache 2.0", url: "http://www.apache.org/licenses/LICENSE-2.0.html" }
-    },
-    tags: [
-      {
-        name: "Fees",
-        description: "Total fees paid to a protocol on a given day.",
-        externalDocs: { description: "Find out more", url: "https://cryptostats.community/discover/fees" },
-        version: '0.1.0'
+export default async function handler( req: NextApiRequest, res: NextApiResponse<any> ) {
+  setCache(res);
+  const spec = createSwaggerSpec({
+    definition: {
+      openapi: '3.0.0',
+      info: {
+        version: pkg.version,
+        title: pkg.title,
+        description: pkg.description,
+        contact: { url: pkg.homepage },
+        license: { name: "Apache 2.0", url: "http://www.apache.org/licenses/LICENSE-2.0.html" }
       },
-      {
-        name: "Issuance",
-        description: "The amount of tokens issued in the past day, in USD.",
-        externalDocs: { description: "Find out more", url: "https://cryptostats.community/discover/issuance" },
-        version: '0.1.0'
-      }
-    ]
-  },
-  schemaFolders: ['models'],
-  apiFolder: 'pages/api',
-});
-export default swaggerHandler();
+      tags: [
+        {
+          name: "Fees",
+          description: "Total fees paid to a protocol on a given day.",
+          externalDocs: { description: "Find out more", url: "https://cryptostats.community/discover/fees" },
+          version: '0.1.0'
+        },
+        {
+          name: "Issuance",
+          description: "The amount of tokens issued in the past day, in USD.",
+          externalDocs: { description: "Find out more", url: "https://cryptostats.community/discover/issuance" },
+          version: '0.1.0'
+        }
+      ]
+    },
+    schemaFolders: ['models'],
+    apiFolder: 'pages/api',
+  });
+  return res.status(200).json(spec);
+}
--- pages/docs.tsx
+++ pages/docs.tsx
@@ -1,6 +1,7 @@
 import dynamic from 'next/dynamic';
 import Head from 'next/head';
 import 'swagger-ui-react/swagger-ui.css';
+import * as pkg from "../package.json";
 
 const SwaggerUI = dynamic<{ url: string }>((import('swagger-ui-react') as any), {
   ssr: false,
@@ -10,8 +11,8 @@ export default function Index() {
   return (
     <div>
       <Head>
-        <title>EOSIO CryptoStats API</title>
-        <meta name="description" content="One neutral source of truth for crypto metrics. Used by everyone, managed by the community." />
+        <title>{ pkg.title }</title>
+        <meta name="description" content={ pkg.description } />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <SwaggerUI url="/api/swagger.json" />
